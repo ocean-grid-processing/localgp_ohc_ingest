6fd51c76acd3424550344ae86bb23e9f972b5bb4
Katie Mills <you@example.com>
2026-08-11 16:04:35 -0600

    production candidate for 260810 run

--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
-for level in 15_20 15_300 300_700 700_1850 1800_1850
+for layer in 15_20 15_300 300_700 700_1000 700_1850 1800_1850
 do
-  declare ingest=$(sbatch --parsable ohc_ingest.slurm $level)
-  sbatch --dependency afterok:$ingest verify_store.slurm $level
-  declare publish=$(sbatch --parsable --dependency afterok:$ingest publish.slurm $level)
-  sbatch --dependency afterok:$publish verify_publish.slurm $level
+  meandir=/scratch/alpine/wimi7695/ohc_prod/potential_temperature/OP20260507/${layer}/OP20260507_potentialTemperature_-89.5S_89.5N_20.5W_379.5E_2004_2025_${layer}/Results/FullField
+  ensembledir=/scratch/alpine/wimi7695/ohc_prod/potential_temperature/OP20260507/${layer}/OP20260507_potentialTemperature_-89.5S_89.5N_20.5W_379.5E_2004_2025_${layer}/Results/FullFieldLocalCondSim
+  outputdir=/scratch/alpine/wimi7695/ohc_prod/results
+  tag=prod_260810
+  declare ingest=$(sbatch --parsable ohc_ingest.slurm $meandir $ensembledir $outputdir $layer $tag)
+  sbatch --dependency afterok:$ingest verify_store.slurm $outputdir $meandir $ensembledir $tag $layer
+  declare publish=$(sbatch --parsable --dependency afterok:$ingest publish.slurm $outputdir $tag $layer)
+  sbatch --dependency afterok:$publish verify_publish.slurm $outputdir/OHC_*${layer}*.nc $meandir $ensembledir
 done
