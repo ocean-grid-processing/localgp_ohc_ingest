f480bda80159d38c9677dd9f870549c9133ddcad
Katie Mills <you@example.com>
2026-09-08 14:28:04 -0600

    rc config

--- run.sh
+++ run.sh
@@ -1,13 +1,21 @@
 for layer in 15_20 15_300 300_700 700_1000 700_1850 1800_1850
 do
+  # config ----
   meandir=/scratch/alpine/wimi7695/ohc_prod/potential_temperature/OP20260507/${layer}/OP20260507_potentialTemperature_-89.5S_89.5N_20.5W_379.5E_2004_2025_${layer}/Results/FullField
   ensembledir=/scratch/alpine/wimi7695/ohc_prod/potential_temperature/OP20260507/${layer}/OP20260507_potentialTemperature_-89.5S_89.5N_20.5W_379.5E_2004_2025_${layer}/Results/FullFieldLocalCondSim
-  outputdir=/scratch/alpine/wimi7695/ohc_prod/results_260824
-  tag=260824-OP20260507
-  # link to the exact ohc_ingest build you deployed (commit or release URL); stamped as code_version.
-  codeversion=https://github.com/argovis/ohc_ingest/commit/REPLACE_WITH_DEPLOYED_SHA
-  declare ingest=$(sbatch --parsable ohc_ingest.slurm $meandir $ensembledir $outputdir $layer $tag $codeversion)
+  outputdir=/scratch/alpine/wimi7695/ohc_prod/results_260908
+  tag=260908-OP20260507
+  provenance=https://github.com/ocean-grid-processing/provenance/blob/main/README.md  # record of interesting facts about $tag
+  codeversion=https://github.com/ocean-grid-processing/localgp_ohc_ingest/releases/tag/1.0.0
+  # end config ----
+  declare ingest=$(sbatch --parsable ohc_ingest.slurm $meandir $ensembledir $outputdir $layer $tag $provenance $codeversion)
   sbatch --dependency afterok:$ingest verify_store.slurm $outputdir $meandir $ensembledir $tag $layer
-  declare publish=$(sbatch --parsable --dependency afterok:$ingest publish.slurm $outputdir $tag $layer $codeversion)
+  declare publish=$(sbatch --parsable --dependency afterok:$ingest publish.slurm $outputdir $tag $layer $provenance $codeversion)
   sbatch --dependency afterok:$publish verify_publish.slurm $outputdir/OHC_*${layer}*.nc $meandir $ensembledir
 done
+
+
+
+
+
+
